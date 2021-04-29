8413bfa01549845f436130b214cf35755af82b45
Abigail Adams <abigail-adams@gmail.com>
2021-04-28 23:45:05 -0700

    adding all api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
-const db = require("../db.json");
-
+let db = require("../db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 router.get("/api/notes", function (req, res) {
+  res.json(db);
+});
+
+router.post("/api/notes", function (req, res) {
+  req.body.id = uuidv4();
+  db.push(req.body);
+  fs.writeFileSync("./db.json", JSON.stringify(db));
+  res.json(db);
+});
+
+router.delete("/api/notes/:id", function (req, res) {
+  db = db.filter((note) => note.id != req.params.id);
+  fs.writeFileSync("./db.json", JSON.stringify(db));
   res.json(db);
 });
 
