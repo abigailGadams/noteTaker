8e259e2a6c3d599c85acdf58940ea3ae375e1e2b
Abigail Adams <abigail-adams@gmail.com>
2021-04-28 22:34:10 -0700

    adding api route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const db = require("../db.json");
+
+router.get("/api/notes", function (req, res) {
+  res.json(db);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
 
 app.listen(PORT, function () {
   console.log("app is listening", PORT);
